1025c8cb1fb8a8a643f72e75115e763e9018c20c
caijunjie <cai.junjie@synyi.com>
2019-10-22 18:54:27 +0800

    type hinting demo

--- 0035_type_hinting/pycharm_demo/better_hiting.py
+++ 0035_type_hinting/pycharm_demo/better_hiting.py
@@ -1,7 +1,8 @@
-def f(x, y):
-# def f(x: int, y: int):
-    return x +  y
+# def f(x, y):
+def f(x: int, y: int):
+    return x + y
 
 
 # 用command + p调出函数接口定义信息，比较type_hinting对提示信息的影响
-f()
+f("a", 1)
+
